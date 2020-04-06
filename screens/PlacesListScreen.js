f92fc544a7b3d7545633c36db51491a0f200d1ac
jirwincline <jci@pdx.edu>
2020-04-06 16:59:03 -0700

    added header button

--- screens/PlacesListScreen.js
+++ screens/PlacesListScreen.js
@@ -1,5 +1,9 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import { Platform } from "react-native";
+
+import HeaderButton from "../components/HeaderButton";
 
 const PlacesListScreen = () => {
   return (
@@ -9,8 +13,21 @@ const PlacesListScreen = () => {
   );
 };
 
-PlacesListScreen.navigationOptions = {
-  headerTitle: "All Places",
+PlacesListScreen.navigationOptions = (navData) => {
+  return {
+    headerTitle: "All Places",
+    headerRight: (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Add Place"
+          iconName={Platform.OS === "android" ? "md-add" : "ios-add"}
+          onPress={() => {
+            navData.navigation.navigate("NewPlace");
+          }}
+        />
+      </HeaderButtons>
+    ),
+  };
 };
 
 const styles = StyleSheet.create({});
