1757dc8e972906d142da1f7cb4a57eeace4daf25
jirwincline <jci@pdx.edu>
2020-04-07 18:54:22 -0700

    add image picker component

---
+++ components/ImagePicker.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
+
+const ImagePicker = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default ImagePicker;
