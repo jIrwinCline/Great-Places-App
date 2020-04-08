cce421ffff42de4636e31a84c3743191db419176
jirwincline <jci@pdx.edu>
2020-04-08 11:43:24 -0700

    add styling to imagePicker

--- components/ImagePicker.js
+++ components/ImagePicker.js
@@ -1,12 +1,43 @@
 import React from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
+import { View, Button, Image, Text, StyleSheet, Alert } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import * as Permissions from "expo-permissions";
 
-const ImagePicker = () => {
+import Colors from "../constants/Colors";
+
+const ImgPicker = (props) => {
   return (
-    <View>
-      <Text></Text>
+    <View style={styles.imagePicker}>
+      <View style={styles.imagePreview}>
+        <Text>No image picked yet.</Text>
+        <Image style={styles.image} />
+      </View>
+      <Button
+        title="Take Image"
+        color={Colors.primary}
+        onPress={takeImageHandler}
+      />
     </View>
   );
 };
 
-export default ImagePicker;
+const styles = StyleSheet.create({
+  imagePicker: {
+    alignItems: "center",
+  },
+  imagePreview: {
+    width: "100%",
+    height: 200,
+    marginBottom: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    borderColor: "#ccc",
+    borderWidth: 1,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
+
+export default ImgPicker;
