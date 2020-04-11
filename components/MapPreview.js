6f532085345ab1e45d474fd5d2ff0e23fcfddc40
jirwincline <jci@pdx.edu>
2020-04-11 16:36:02 -0700

    add choosable markers into the map that lives inside the map screen

--- components/MapPreview.js
+++ components/MapPreview.js
@@ -1,24 +1,29 @@
 import React from "react";
-import { View, Text, Image, StyleSheet } from "react-native";
+import { TouchableOpacity, Image, StyleSheet } from "react-native";
+
 import ENV from "../env";
 
 const MapPreview = (props) => {
   let imagePreviewUrl;
 
   if (props.location) {
-    imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${props.location.lat},${props.location.lng}&zoom=13&size=400x200&maptype=roadmap&markers=color:red%7Clabel:A%7C${props.location.lat},${props.location.lng}&key=${ENV.googleApiKey}`;
+    imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${props.location.lat},${props.location.lng}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:A%7C${props.location.lat},${props.location.lng}&key=${ENV.googleApiKey}`;
   }
 
   return (
-    <View style={{ ...styles.mapPreview, ...props.style }}>
+    <TouchableOpacity
+      onPress={props.onPress}
+      style={{ ...styles.mapPreview, ...props.style }}
+    >
       {props.location ? (
         <Image style={styles.mapImage} source={{ uri: imagePreviewUrl }} />
       ) : (
-        props.childeren
+        props.children
       )}
-    </View>
+    </TouchableOpacity>
   );
 };
+
 const styles = StyleSheet.create({
   mapPreview: {
     justifyContent: "center",
@@ -29,4 +34,5 @@ const styles = StyleSheet.create({
     height: "100%",
   },
 });
+
 export default MapPreview;
